7b476d3ed872228f8d7d44fcdacbad87a88a132d
Julian Grinblat <julian@dotcore.co.il>
2021-06-27 21:50:59 +0900

    Fix setup for symlinks

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ git submodule update --init --recursive
 
 qmk_firmware/util/qmk_install.sh
 
-ln -sf "$DIR/dactyl_manuform" qmk_firmware/keyboards/handwired/dactyl_manuform/4x5/keymaps/perrin4869
+# symlinks don't work, #include fails
+KEYMAP_DIR=qmk_firmware/keyboards/handwired/dactyl_manuform/4x5/keymaps/perrin4869
+rm -rf $KEYMAP_DIR
+cp -rf "$DIR/dactyl_manuform" $KEYMAP_DIR
