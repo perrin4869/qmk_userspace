34e071e939dfba97606dcaf757ac76daf5f0a43d
Julian Grinblat <julian@dotcore.co.il>
2025-10-22 18:50:43 +0900

    chore: test if a workflow is dispatched

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/perrin4869/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/perrin4869/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                                         KC_F,   KC_G,   KC_C,    KC_R,   KC_L,
         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                                         KC_D,   KC_H,   KC_T,    KC_N,   KC_S,
-        KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                         KC_B,   KC_M,   KC_W,    KC_V,   KC_Z,
+        KC_SCLN, KC_O,    KC_J,    KC_K,    KC_X,                                         KC_B,   KC_M,   KC_W,    KC_V,   KC_Z,
                  KC_LBRC, KC_RBRC,                                                                        KC_MINS, KC_EQL,
                                    ALT_ESC, SFT_ENT,                                      SFT_SPC, CTL_BSPC,
                                                      RAISE_TAB, KC_LGUI,  KC_RGUI, LOWER_DEL,
